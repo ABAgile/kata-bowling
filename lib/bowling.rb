838a8b9fbabed6b6b66af8c9e221fa4f0cc40e9e
Szeto Bo <szetobo@gmail.com>
2018-05-07 00:02:52 +0800

    Step 1: all missed score 0

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,3 +1,12 @@
 # class for American Ten-Pin Bowling game score calculation
 class Bowling
+  attr_reader :frames
+
+  def roll(frames)
+    @frames = frames
+  end
+
+  def score
+    0
+  end
 end
--- test/bowling_test.rb
+++ test/bowling_test.rb
@@ -4,4 +4,10 @@ require 'minitest/pride'
 require_relative '../lib/bowling'
 
 describe Bowling do
+  subject { Bowling.new }
+
+  it 'should score 0 for all missed' do
+    subject.roll('-' * 20)
+    assert_equal 0, subject.score
+  end
 end
